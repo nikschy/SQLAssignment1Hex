98bec85bbefaec44179e294400fa0e80ef9dd275
nikschy <162898945+nikschy@users.noreply.github.com>
2024-05-02 00:44:21 +0530

    Add files via upload

---
+++ SQLAssignment1.py
@@ -0,0 +1,29 @@
+import sqlite3
+
+db = sqlite3.connect(":memory:")
+
+cursor = db.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS AGES(
+NAME VARCHAR(50),
+AGE INTEGER
+)''')
+
+
+cursor.execute('''DELETE FROM AGES''')
+
+cursor.execute('''INSERT INTO AGES (NAME,AGE) VALUES('Mara',28)''')
+cursor.execute('''INSERT INTO AGES (NAME,AGE) VALUES('Otto',33)''')
+cursor.execute('''INSERT INTO AGES (NAME,AGE) VALUES('Fyn',31)''')
+cursor.execute('''INSERT INTO AGES (NAME,AGE) VALUES('Neshawn',17)''')
+
+
+cursor.execute('''SELECT HEX(NAME||AGE) AS X FROM AGES ORDER BY X''')
+
+
+user1 = cursor.fetchone()
+print("The first row in the resulting record set: " + user1[0])
+
+db.commit()
+db.close()
